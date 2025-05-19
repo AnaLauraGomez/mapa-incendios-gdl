e768edb4a72fc94e388b79a26e23707314f81cc5
Gab_5 <gabrielarambula05@gmail.com>
2025-05-19 00:20:54 -0600

    Correción en ponderación de grafo y hacer que las funciones filtren solo los bosques

--- src/main.py
+++ src/main.py
@@ -1,40 +1,42 @@
-from simulacion import crear_grafo, propagacion_bfs, propagacion_dfs, detectar_zonas_criticas,detectar_zonas_criticas_peso
+from simulacion import crear_grafo, propagacion_bfs, propagacion_dfs,detectar_zonas_criticas_peso
 
 def main():
     grafo = crear_grafo()  #Creamos el grafo
+    bosques = ['Colomos', 'Primavera', 'Huentitán', 'Mirador']
 
     while True:
         print("\nMapa de la red de bosques:\n")
         for zona, conexiones in grafo.items():
-            print(f"{zona}: {[f'{vecino} (peso: {peso})' for vecino, peso in conexiones]}")
+            if zona in bosques:
+                print(f"{zona}: {[f'{vecino} (peso: {peso})' for vecino, peso in conexiones]}")
+
         print("\n <--- Simulacion de propagacion de fuego --->")
         print("1. Propagacion BFS")
         print("2. Propagacion DFS")
         print("3. Deteccion de zonas criticas")
-        print("4. Deteccion de zonas criticas (por ponderacion)")
-        print("5. Salir")
+        print("4. Salir")
 
         opcion = input("Seleccione una opcion (1-5):\n")
 
         if opcion == '1':
             inicio = input("\nIngrese la zona de inicio para la propagacion BFS:\n")
-            if inicio in grafo:
+            if inicio in bosques:
                 propagacion_bfs(grafo,inicio)
             else:
                 print("\nBosque no encontrado en la red. Intente de nuevo.")
         elif opcion == '2':
             inicio = input("\nIngrese la zona de inicio para la propagacion DFS: \n")
-            if inicio in grafo:
+            if inicio in bosques:
                 propagacion_dfs(grafo,inicio)
             else:
                 print("\nBosque no encontrado en la red. Intente de nuevo.")
         elif opcion == '3':
-            detectar_zonas_criticas(grafo)
-        elif opcion == '4':
             detectar_zonas_criticas_peso(grafo)
-        elif opcion == '5':
-            print("\nSaliendo de la simulacion...")
-            break
+        elif opcion == '4':
+             print("\nSaliendo de la simulacion...")
+             break
+        else:
+            print("\nOpcion no valida. Intente de nuevo.")
     
 if __name__ == "__main__":
     main()
--- src/simulacion.py
+++ src/simulacion.py
@@ -12,34 +12,38 @@ grafo = {
 
     #Ejemplo: 
     #Un incendio que inicia en Colomos: 
-    # Tardará moderadamente en llegar a Primavera (peso 3). 
-    # Tardará más en llegar a Huentitán (peso 6).
+    # Tardará más en llegar a Primavera (peso 3). 
+    # Tardará mucho menos en llegar a Huentitán (peso 5).
 
-   # | Peso | Significado                  
-    #| 1    | Propagación muy fácil            
-    #| 2–3  | Propagación fácil              
-    #| 4–5  | Propagación media                 
-    #| 6–7  | Propagación difícil               
-    #| 8+   | Muy difícil o casi imposible      
+"""Para aplica ponderación, hicimos uso de la formula inversa  simple donde
+   el peso de la arista es inversamente proporcional a la distancia entre los nodos.
+   Por lo tanto, a mayor distancia entre los nodos, menor será el peso de la arista."""    
 
 #Función principal que crea el grafo con las zonas y conexiones
 #Cada zona es un nodo y cada conexión representa una posible ruta de propagación de fuego
 #Los numeros indican qué tan facil o dificil es que el fuego se propague por esa ruta
 def crear_grafo():
     grafo = {
-        'Colomos': [('Primavera', 3), ('Huentitán', 6), ('Mirador', 7), ('Bomberos_PabloNeruda', 2), ('Bomberos_Central', 5)],
-        'Primavera': [('Colomos', 3), ('Huentitán', 5), ('Mirador', 4), ('Bomberos_Zapopan4', 3)],
-        'Huentitán': [('Colomos', 6), ('Primavera', 5), ('Mirador', 8), ('Bomberos_Zapopan2', 4), ('Bomberos_Base2', 5)],
-        'Mirador': [('Colomos', 7), ('Primavera', 4), ('Huentitán', 8), ('Bomberos_Central', 4)],
+        #Bosques
+        'Colomos': [('Primavera', 3), ('Huentitán', 5), ('Mirador', 8)],
+        'Primavera': [('Colomos', 3), ('Huentitán', 1), ('Mirador', 2)],
+        'Huentitán': [('Colomos', 5), ('Primavera', 1), ('Mirador', 8)],
+        'Mirador': [('Colomos', 8), ('Primavera', 2), ('Huentitán', 8)],
 
-        #Estaciones de bomberos
-        'Bomberos_Central': [('Colomos', 5), ('Mirador', 4)],
-        'Bomberos_Base2': [('Huentitán', 5)],
-        'Bomberos_PabloNeruda': [('Colomos', 2)],
-        'Bomberos_Zapopan1': [('Primavera', 4)],
-        'Bomberos_Zapopan2': [('Huentitán', 4)],
-        'Bomberos_Zapopan3': [('Primavera', 5)],
-        'Bomberos_Zapopan4': [('Primavera', 3)]
+        #Estaciones de bomberos conectadas a los bosques
+        #Cuartel Central
+        'Cuartel Central':[('Colomos',11),('Huentitán', 6),('Primavera', 3),('Mirador',9)],
+        #Unidad Estatal de Protección Civil y Bomberos Jalisco 
+        'UEPCBJ':[('Colomos',7),('Huentitán', 4),('Primavera', 3),('Mirador',6)],
+        #Coordinación Municipal De Protección Civil Y Bomberos De Zapopan (Base 1) Oficinas Administrativas
+        'Estación1':[('Colomos',19),('Huentitán', 5),('Primavera', 2),('Mirador',9)],
+        #Base 2 Protección Civil y Bomberos Zapopan, Los Molinos
+        'Estacion2':[('Colomos',4),('Huentitán', 3),('Primavera', 2),('Mirador',3)],
+        #Base 3 Protección Civil y Bomberos Zapopan, Auditorio
+        'Estacion3':[('Colomos',12),('Huentitán', 6),('Primavera', 2),('Mirador',14)],
+        #Coordinación Municipal De Protección Civil Y Bomberos De Zapopan (Base 4)
+        'Estacion4':[('Colomos',6),('Huentitán', 3),('Primavera', 4),('Mirador',4)]
+        
     }
     return grafo
 
@@ -48,6 +52,11 @@ def crear_grafo():
 #Utiliza una cola para recorrer los nodos en orden de aparicion
 
 def propagacion_bfs(grafo, inicio):
+    bosques = ['Colomos', 'Primavera', 'Huentitán', 'Mirador']
+    if inicio not in bosques:
+        print(f"\nZona no válida. Debe ser una de las siguientes: {bosques}")
+        return
+    
     cola = [inicio]
     visitados = []   #Lista para guardar las zonas ya visitadas
 
@@ -60,7 +69,7 @@ def propagacion_bfs(grafo, inicio):
             visitados.append(actual)  #Marcamos la zona como visitada
 
             for vecino, peso in grafo[actual]:  #Conexiones de esa zona
-                if vecino not in visitados and vecino not in cola:
+                if vecino in bosques and vecino not in visitados and vecino not in cola:
                     print("\nSe propaga a: ", vecino, " , con peso: ", peso)
                     cola.append(vecino)  #Agregamos el vecino a la cola para despues
 
@@ -69,6 +78,11 @@ def propagacion_bfs(grafo, inicio):
 #Utiliza recursion y una lista de visitados para no repetir zonas
 
 def propagacion_dfs(grafo, inicio, visitados=None):
+    bosques = ['Colomos', 'Primavera', 'Huentitán', 'Mirador']
+    if inicio not in bosques:
+        print(f"\nZona no válida. Debe ser una de las siguientes: {bosques}")
+        return
+    
     if visitados is None:
         visitados = []  #Lista de zonas visitadas
 
@@ -77,27 +91,21 @@ def propagacion_dfs(grafo, inicio, visitados=None):
         visitados.append(inicio)  #Marcamos como visitada
 
         for vecino, peso in grafo[inicio]:  #Recorremos los vecinos
-            if vecino not in visitados:
+            if vecino in bosques and vecino not in visitados:
                 print("\nSe propaga a: ", vecino, " , con peso: ", peso) 
                 propagacion_dfs(grafo, vecino, visitados)  #Llamada recursiva para seguir por ese camino
 
 
-#Funcion para detectar zonas criticas en el grafo
-#Una zona critica es aquella que esta conectada a muchas otras con un alto riesgo de propagación
 
-def detectar_zonas_criticas(grafo, umbral=3):
-    print("\n[Detección de zonas criticas]")
-    for zona in grafo:
-        conexiones = len(grafo[zona])  #Numero de vecinos
-        if conexiones >= umbral:
-            print(f"- {zona} es una zona critica (conectada a {conexiones} zonas)")
+"""Función que detecta zonas criticas mediante las aristas de los nodos,
+   se basa en la ponderacion de las aristas. Una zona critica es aquella que tiene un peso total de conexiones menor a un umbral dado, la zona es
+   aquella que tiene un alto riesgo de propagación de fuego."""
 
-"""Función que hace lo mismo que la función anterior solo que en lugar de basarse en las aristas de los nodos,
-   se basa en la ponderacion de las aristas"""
-
-def detectar_zonas_criticas_peso(grafo, umbral=17):
+def detectar_zonas_criticas_peso(grafo, umbral=15):
+    bosques = ['Colomos', 'Primavera', 'Huentitán', 'Mirador']
     print("\n <--- Deteccion de zonas criticas por ponderacion entre conexiones --->")
-    for zona, conexiones in grafo.items():
+    for zona in bosques:
+        conexiones = grafo.get(zona, [])
         peso_total = sum(peso for _, peso in conexiones)
-        if peso_total >= umbral:
+        if peso_total > umbral:
             print(f"- {zona} es una zona critica (peso total de conexiones: {peso_total})")
